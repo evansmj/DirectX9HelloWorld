99e551822b677ec015706d33400934eee20d80ef
evansmj <evansmj@dukes.jmu.edu>
2016-06-19 03:03:36 -0400

    Draw triangle

--- winmain.cpp
+++ winmain.cpp
@@ -12,6 +12,24 @@
 //Include our D3DApp wrapper class
 #include "DXApp.h"
 
+struct VertexPositionColor
+{
+	VertexPositionColor() {}
+	VertexPositionColor(float _x, float _y, float _z, D3DCOLOR c)
+	{
+		x = _x;
+		y = _y;
+		z = _z;
+		color = c;
+	}
+
+	float x, y, z;
+	D3DCOLOR color;
+	static const DWORD FVF; // flexible vretex format;
+};
+
+const DWORD VertexPositionColor::FVF = D3DFVF_XYZ | D3DFVF_DIFFUSE; //diffuse means color
+
 class TestApp : public DXApp
 {
 public:
@@ -27,6 +45,9 @@ public:
 	void OnLostDevice() override;
 	void OnResetDevice() override;
 };
+
+IDirect3DVertexBuffer9 * VB; //gpu reads vertices after binded here
+IDirect3DIndexBuffer9 * IB; //tells the order to read them^
 
 //Calls the base class (DXApp) constructor
 TestApp::TestApp(HINSTANCE hInstance):DXApp(hInstance)
@@ -47,7 +68,54 @@ bool TestApp::Init()
 	if(!DXApp::Init())
 		return false;
 
-	//Else return true
+	VertexPositionColor verts[3] =
+	{
+		VertexPositionColor(0.0f, 0.0f, 0.0f, d3dColors::LIME),
+		VertexPositionColor(1.0f, -1.0f, 0.0f, d3dColors::BLUE),
+		VertexPositionColor(-1.0f, -1.0f, 0.0f, d3dColors::RED)
+	};
+
+	//now create vertex buffer
+
+	m_pDevice3D->CreateVertexBuffer(3 * sizeof(VertexPositionColor), 0, VertexPositionColor::FVF, D3DPOOL_MANAGED,
+		&VB, NULL);
+
+	//how do you manage the vb?  how to add stuff to it?
+
+	VOID * pVerts;
+
+	//have to lock the vb before doing anything
+	VB->Lock(0, sizeof(verts), (void**)&pVerts, 0);
+	memcpy(pVerts, verts, sizeof(verts));
+	VB->Unlock();
+
+	//now all 3 vertices are set to the vertex buffer
+
+	//now set up the camera, since we cant just render without knowing where to view from
+
+	D3DXMATRIX view;
+	D3DXMATRIX proj;
+
+	//set view
+	D3DXVECTOR3 position = D3DXVECTOR3(0.0f, 0.0f, -5.0f);
+	D3DXVECTOR3 target = D3DXVECTOR3(0.0f, 0.0f, 1.0f);
+	D3DXVECTOR3 up = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+
+	D3DXMatrixLookAtLH(&view, &position, &target, &up);
+
+	m_pDevice3D->SetTransform(D3DTS_VIEW, &view);
+
+	//set projection
+	D3DXMatrixPerspectiveFovLH(&proj, D3DX_PI / 4, static_cast<float>(m_ClientWidth)/m_ClientHeight, 1.0f, 1000.0f);
+	m_pDevice3D->SetTransform(D3DTS_PROJECTION, &proj);
+
+	//projecection matrix defines how camera view the world, fov 180 degrees etc
+
+	//view matrix is the orientation of that ^ view.  what change sbased on rotation etc.  where up is
+
+	m_pDevice3D->SetRenderState(D3DRS_LIGHTING, false);
+	m_pDevice3D->SetRenderState(D3DRS_SHADEMODE, D3DSHADE_GOURAUD);
+
 	return true;
 }
 
@@ -63,9 +131,15 @@ void TestApp::Render()
 {
 	//D3DCOLOR: Cornflower Blue = RGB(100, 149, 237)
 	//Clears the back buffer
-	m_pDevice3D->Clear(0, 0, D3DCLEAR_TARGET, d3dColors::CORN_FLOWER_BLUE, 1.0f, 0);
+	m_pDevice3D->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, d3dColors::CORN_FLOWER_BLUE, 1.0f, 0);
 
+	//need to call begin scene and end scene before rendering
+	m_pDevice3D->BeginScene();
+	m_pDevice3D->SetStreamSource(0, VB, 0, sizeof(VertexPositionColor));
+	m_pDevice3D->SetFVF(VertexPositionColor::FVF);
+	m_pDevice3D->DrawPrimitive(D3DPT_TRIANGLELIST, 0, 1);
 
+	m_pDevice3D->EndScene();
 
 	//Present the backbuffer to our window
 	m_pDevice3D->Present(0, 0, 0, 0);
